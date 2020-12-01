359e398c6c16da2f17c708853e4b53c5fef24995
andres velasco <velasco.andrs@gmail.com>
2020-11-30 23:24:11 -0500

    add online column on user entity

--- secret-chat-backend/src/modulos/usuario/usuario.entity.ts
+++ secret-chat-backend/src/modulos/usuario/usuario.entity.ts
@@ -2,17 +2,23 @@ import { AbstractMongoEntity } from '@pimba/excalibur/lib';
 import { Column, Entity, Index } from 'typeorm';
 
 @Entity('usuario')
-@Index(['username'],{unique: true})
 export class UsuarioEntity extends AbstractMongoEntity {
   @Column()
-  @Index({unique: true})
+  @Index({ unique: true })
   username: string;
 
   @Column()
-  @Index({unique: true})
+  @Index({ unique: true })
   email: string;
-
 
   @Column()
   password: string;
+
+  @Column(
+    {
+      default: false,
+      nullable: true,
+    }
+  )
+  online: boolean;
 }
